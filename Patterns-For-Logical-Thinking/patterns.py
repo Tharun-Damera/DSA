7add19ebf1bba3aef10d9f62081cbdc42e73ebc9
Tharun-Damera <tharun.damera4@gmail.com>
2023-05-18 21:10:40 +0530

    add patterns 3,4,5,6

--- Patterns-For-Logical-Thinking/patterns.py
+++ Patterns-For-Logical-Thinking/patterns.py
@@ -13,7 +13,7 @@ Output:
 """
 def pattern1(n):
 	print("Pattern-1: ")
-	for i in range(n):
+	for i in range(n):	
 		for j in range(n):
 			print('*', end=' ')
 		print()
@@ -40,10 +40,109 @@ def pattern2(n):
 		print()
 
 
+"""
+Pattern – 3: Right-Angled Number Pyramid
+
+Input Format: N = 6
+Output:
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+
+"""
+def pattern3(n):
+	print("Pattern-3: ")
+	for i in range(1, n+1):
+		for j in range(1, i+1):
+			print(j, end=' ')
+		print()
 
 
+"""
+Pattern – 4: Right-Angled Number Pyramid – II
+
+Input Format: N = 6
+Output:
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+
+"""
+def pattern4(n):
+	print("Pattern-4: ")
+	for i in range(1, n+1):
+		for j in range(1, i+1):
+			print(i, end=' ')
+		print()
+
+
+"""
+Pattern-5: Inverted Right Pyramid
+
+Input Format: N = 6
+Output:
+* * * * * *
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+"""
+def pattern5(n):
+	print("Pattern-5: ")
+	for i in range(n):
+		for j in range(n-i):
+			print('*', end=' ')
+		print()
+
+
+"""
+Pattern – 6: Inverted Numbered Right Pyramid
+
+Input Format: N = 6
+Output:
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2 
+1
+
+"""
+def pattern6(n):
+	print("Pattern-6: ")
+	for i in range(n):
+		for j in range(1, n-i+1):
+			print(j, end=' ')
+		print()
+
+
+"""
+Pattern – 7: Star Pyramid
+
+Input Format: N = 6
+Output:
+     *     
+    ***    
+   *****   
+  *******  
+ ********* 
+***********
+
+"""
+def pattern7(n):
+	print("Pattern-7: ")
+	for i in range(n):
+		pass
 
 
 num = int(input("Enter n value: "))
 
-pattern2(num)
+pattern6(num)
