2a875ab69ee08fd2d4cc11857f26a67f3741fdaf
Tharun-Damera <tharun.damera4@gmail.com>
2023-05-19 16:42:22 +0530

    add pattern 8,9,10,11

--- Patterns-For-Logical-Thinking/patterns.py
+++ Patterns-For-Logical-Thinking/patterns.py
@@ -150,10 +150,294 @@ def pattern7(n):
 		print()
 
 
+"""
+Pattern – 8: Inverted Star Pyramid
+
+Input Format: N = 6
+Output:
+***********
+ *********
+  *******
+   ***** 
+    ***    
+     *
+
+"""
+def pattern8(n):
+	print("Pattern-8: ")
+	for i in range(n):
+		for j in range(i):
+			print(' ', end='')
+		for j in range(2*n - (2*i+1)):
+			print('*', end='')
+		for j in range(i):
+			print(' ', end='')
+		print()
+
+
+"""
+Pattern – 9: Diamond Star Pattern
+
+Input Format: N = 6
+Output:
+     *
+    ***
+   ***** 
+  *******
+ *********
+***********  
+***********
+ *********
+  *******
+   ***** 
+    ***    
+     *
+
+"""
+def pattern9(n):
+	print("Pattern-9: ")
+	for i in range(n):
+		for j in range(n-i-1):
+			print(' ', end='')
+		for j in range(2*i+1):
+			print('*', end='')
+		for j in range(n-i-1):
+			print(' ', end='')
+
+		print()
+	for i in range(n):
+		for j in range(i):
+			print(' ', end='')
+		for j in range(2*n - (2*i+1)):
+			print('*', end='')
+		for j in range(i):
+			print(' ', end='')
+		print()
+
+
+"""
+Pattern – 10: Half Diamond Star Pattern
+
+Input Format: N = 6
+Output:
+     *
+     **
+     *** 
+     ****
+     *****
+     ******  
+     *****
+     ****
+     ***    
+     **
+     *
+
+"""
+def pattern10(n):
+	print("Pattern-10: ")
+	for i in range(1, 2*n):
+		stars = i if i <= n else 2*n-i
+
+		for j in range(stars):
+			print('*', end='')
+		print()
+
+"""
+Pattern – 11: Binary Number Triangle Pattern
+
+Input Format: N = 6
+Output:
+1
+01
+101
+0101
+10101
+010101
+
+"""
+def pattern11(n):
+	print("Pattern-11: ")
+	for i in range(n):
+		for j in range(i+1):
+			if (i+j) % 2 == 0:
+				print('1', end='')
+			else:
+				print('0', end='')
+		print()
 
 
 
+"""
+Pattern – 12: Number Crown Pattern
+
+Input Format: N = 6
+Output:
+1          1
+12        21
+12       321
+1234    4321
+12345  54321
+123456654321
+
+"""
+
+
+"""
+Pattern – 13: Increasing Number Triangle Pattern
+
+Input Format: N = 6
+Output:
+1
+2  3
+4  5  6
+7  8  9  10
+11  12  13  14  15
+16  17  18  19  20  21
+
+"""
+
+"""
+Pattern – 14: Increasing Letter Triangle Pattern
+
+Input Format: N = 6
+Output:
+A
+A B
+A B C
+A B C D
+A B C D E
+A B C D E F
+
+"""
+
+"""
+Pattern – 15: Reverse Letter Triangle Pattern
+
+Input Format: N = 6
+Output:
+A B C D E F
+A B C D E 
+A B C D
+A B C
+A B
+A
+
+"""
+
+"""
+Pattern – 16: Alpha-Ramp Pattern
+
+Input Format: N = 6
+Output:
+A 
+B B
+C C C
+D D D D
+E E E E E
+F F F F F F
+
+"""
+
+"""
+Pattern – 17: Alpha-Hill Pattern
+
+Input Format: N = 6
+Output:
+     A     
+    ABA    
+   ABCBA   
+  ABCDCBA  
+ ABCDEDCBA 
+ABCDEFEDCBA
+
+"""
+
+"""
+Pattern – 18: Alpha-Triangle Pattern
+
+Input Format: N = 6
+Output:
+F
+E F
+D E F
+C D E F
+B C D E F
+A B C D E F
+
+"""
+
+"""
+Pattern – 19: Symmetric-Void Pattern
+
+Input Format: N = 6
+Output:
+************
+*****  *****
+****    ****
+***      ***
+**        **
+*          *
+*          *
+**        **
+***      ***
+****    ****
+*****  *****
+************
+
+"""
+
+"""
+Pattern – 20: Symmetric-Butterfly Pattern
+
+Input Format: N = 6
+Output:
+*          *
+**        **
+***      ***
+****    ****
+*****  *****
+************
+*****  *****
+****    ****
+***      ***
+**        **
+*          *
+
+"""
+
+"""
+Pattern – 21: Hollow Rectangle Pattern
+
+Input Format: N = 6
+Output:
+******
+*    *
+*    *
+*    *
+*    *
+******
+
+"""
+
+"""
+Pattern – 22: The Number Pattern
+
+Input Format: N = 6
+Output:
+6 6 6 6 6 6 6 6 6 6 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 2 1 2 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 6 6 6 6 6 6 6 6 6 6
+
+"""
 
 num = int(input("Enter n value: "))
 
-pattern7(num)
+pattern11(num)
