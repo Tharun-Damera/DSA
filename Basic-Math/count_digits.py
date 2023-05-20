f6df0b731f05d676afabfdd538ce7cf0b14b2826
Tharun-Damera <tharun.damera4@gmail.com>
2023-05-20 16:51:27 +0530

    create a folder basic math with a problem count_digits

---
+++ Basic-Math/count_digits.py
@@ -0,0 +1,18 @@
+"""
+	Count digits in a number
+	Problem Statement: Given an integer N , write program to count number of digits in N.
+
+"""
+
+def count_digits(num):
+	n = num
+	cnt = 0
+	while n != 0:
+		n = n // 10
+		cnt += 1
+
+	return cnt
+
+
+num = int(input("Enter a number: "))
+print('Number of digits:', count_digits(num))
