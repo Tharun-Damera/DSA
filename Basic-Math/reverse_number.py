99341df3fb610e0a604ccb333ed9147b8c1fca83
Tharun-Damera <tharun.damera4@gmail.com>
2023-05-21 19:06:54 +0530

    add reverse number problem

---
+++ Basic-Math/reverse_number.py
@@ -0,0 +1,20 @@
+'''
+    Reverse a number in C.
+
+    Problem Statement: Given a number N reverse the number and print it.
+
+'''
+
+def reverse_num(num):
+    temp = num
+    rev = 0 
+    while temp > 0:
+        digit = temp % 10
+        rev = rev * 10 + digit
+        temp = temp // 10
+    
+    return rev
+
+
+n = int(input('Enter a number: '))
+print(f'The reverse of the number {n} is {reverse_num(n)}.')
