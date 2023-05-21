47a0dbdb33f6228d4b866686152b7035a0ad9407
Tharun-Damera <tharun.damera4@gmail.com>
2023-05-21 19:21:36 +0530

    add armstrong problem

---
+++ Basic-Math/armstrong.py
@@ -0,0 +1,24 @@
+'''
+	Check if a number is Armstrong Number or not
+	Problem Statement: Given a number, check if it is Armstrong Number or Not
+
+'''
+
+def is_armstrong(num):
+    power = len(str(num))
+    temp = num
+    ans = 0
+    while temp > 0:
+        digit = temp % 10
+        ans += digit ** power
+        temp = temp // 10
+    
+    return num == ans
+    
+
+num = int(input('Enter a number: '))
+ans = is_armstrong(num)    
+if ans == True:
+    print('It is an armstrong number')
+else:
+    print('It is not an armstrong number') 
