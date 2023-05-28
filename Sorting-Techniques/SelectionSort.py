739e67a0143df6607143db788b81752fac61a4f2
Tharun-Damera <tharun.damera4@gmail.com>
2023-05-28 10:42:47 +0530

    add selection sort algorithm

---
+++ Sorting-Techniques/SelectionSort.py
@@ -0,0 +1,32 @@
+'''
+	Selection Sort Algorithm
+	Problem Statement: Given an array of N integers, write a program to implement the Selection sorting algorithm.
+
+'''
+
+def selection_sort(arr):
+
+	n = len(arr)
+
+	for i in range(n-1):
+		min_index = i
+
+		for j in range(i+1, n):
+
+			if arr[j] < arr[min_index]:
+				min_index = j
+
+		arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+
+arr = [int(i) for i in input('\nEnter array elements (space separated): ').split()]
+selection_sort(arr)
+print('After Sorting using Selection Sort the array is', arr)
+
+
+'''
+	Time Complexity: O(n^2)
+	Space Complexity: O(1)
+
+'''
