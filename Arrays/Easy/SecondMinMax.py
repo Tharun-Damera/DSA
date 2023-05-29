ca4f0609277ae63b267cf3e9aa69648fa074d6a6
Tharun-Damera <tharun.damera4@gmail.com>
2023-05-29 21:18:09 +0530

    add second largest and smallest number in array problem

---
+++ Arrays/Easy/SecondMinMax.py
@@ -0,0 +1,53 @@
+'''
+	Find Second Smallest and Second Largest Element in an array
+	Problem Statement: Given an array, find the second smallest and second largest element in the array. Print ‘-1’ in the event that either of them doesn’t exist.
+
+'''
+
+import sys
+
+def second_min_max(arr):
+
+	n = len(arr)
+	if n < 4:
+		return -1, -1
+
+	first_max = sec_max = ~sys.maxsize
+	first_min = sec_min = sys.maxsize
+
+
+	for num in arr:
+
+		if num > first_max:
+			sec_max = first_max
+			first_max = num
+
+		if num > sec_max and num != first_max:
+			sec_max = num
+
+		if num < first_min:
+			sec_min = first_min
+			first_min = num
+			
+		if num < sec_min and num != first_min:
+			sec_min = num
+
+
+	return sec_min, sec_max
+
+
+
+
+arr = [int(i) for i in input('\nEnter array elements (space separated): ').split()]
+
+sec_min, sec_max = second_min_max(arr)
+
+print(f'\nSecond Min Value = {sec_min}')
+print(f'Second Max Value = {sec_max}')
+
+
+'''
+	Time Complexity: O(n)
+	Space Complexity: O(1)
+
+'''
