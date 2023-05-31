efdb4603f548b8d9b14335482739561ee953be81
Tharun-Damera <tharun.damera4@gmail.com>
2023-05-31 16:55:44 +0530

    add find missing number in array problem (Brute force approach)

---
+++ Arrays/Easy/MissingNumber.py
@@ -0,0 +1,43 @@
+'''
+	Find the missing number in an array
+	Problem Statement: Given an integer N and an array of size N-1 containing N-1 numbers between 1 to N. 
+	Find the number(between 1 to N), that is not present in the given array.
+
+'''
+
+# 1. Brute Force
+
+def find_missing_num(arr, n):
+
+	for i in range(1, n+1):
+		flag = 0
+
+		for num in arr:    # Linear search
+			if num == i:
+				# element found 
+				flag = 1
+				break
+
+		if flag == 0:
+			# element not found -> missing num
+			return i
+
+	return -1
+
+'''
+	Time Complexity: O(n^2) 
+	Space Complexity: O(1)
+
+'''
+
+
+
+
+
+
+arr = [int(i) for i in input('\nEnter array elements of A (space separated): ').split()]
+n = int(input('Enter n value: '))
+
+missing_num = find_missing_num(arr, n)
+
+print(f'The missing number in array: {arr} is {missing_num}.')
