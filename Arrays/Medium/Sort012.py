3d1b63d36396258df18d72479a2ba0c1f40e16d0
Tharun-Damera <tharun.damera4@gmail.com>
2023-06-06 19:03:56 +0530

    add brute force approach of sort arr of 0s, 1s and 2s problem

---
+++ Arrays/Medium/Sort012.py
@@ -0,0 +1,30 @@
+'''
+	Sort an array of 0s, 1s and 2s
+
+	Problem Statement: Given an array consisting of only 0s, 1s, and 2s. 
+	Write a program to in-place sort the array without using inbuilt sort functions.
+
+'''
+
+# 1. Brute Force
+
+def sort_arr(arr):
+
+	arr.sort()
+	return
+
+'''
+	Time Complexity: O(nlogn) 
+	Space Complexity: O(1) 	-> extra space for the dictionary
+
+'''
+
+
+
+arr = [int(i) for i in input('\nEnter array elements (space separated): ').split()]
+
+print('\nBefore Sorting arr: ', arr)
+
+sort_arr(arr)
+
+print('After Sorting arr: ', arr)
