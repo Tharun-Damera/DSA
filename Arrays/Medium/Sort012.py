68387e5191a52f3f1a696fb10823b19b8cfb3d63
Tharun-Damera <tharun.damera4@gmail.com>
2023-06-06 19:16:10 +0530

    add better approach of sort 0s, 1s & 2s problem

--- Arrays/Medium/Sort012.py
+++ Arrays/Medium/Sort012.py
@@ -8,10 +8,10 @@
 
 # 1. Brute Force
 
-def sort_arr(arr):
+# def sort_arr(arr):
 
-	arr.sort()
-	return
+# 	arr.sort()
+# 	return
 
 '''
 	Time Complexity: O(nlogn) 
@@ -21,6 +21,42 @@ def sort_arr(arr):
 
 
 
+# 2. Better Approach	--> [ Count 0s, 1s & 2s ]
+
+def sort_arr(arr):
+
+	n = len(arr)
+	cnt_0 = cnt_1 = cnt_2 = 0
+
+	for num in arr:
+
+		if num == 0:
+			cnt_0 += 1
+
+		elif num == 1:
+			cnt_1 += 1
+
+		else:
+			cnt_2 += 1
+
+
+	for i in range(n):
+		if i < cnt_0:
+			arr[i] = 0
+
+		elif i < cnt_0 + cnt_1:
+			arr[i] = 1
+
+		else:
+			arr[i] = 2
+
+
+'''
+	Time Complexity: O(n) 
+	Space Complexity: O(1) 
+
+'''
+
 
 arr = [int(i) for i in input('\nEnter array elements (space separated): ').split()]
 
@@ -28,4 +64,4 @@ print('\nBefore Sorting arr: ', arr)
 
 sort_arr(arr)
 
-print('After Sorting arr: ', arr)
+print('After Sorting  arr: ', arr)
